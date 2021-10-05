cd079547ca7843821d0f8354d19ae7f75eafd6c8
DarrenDee96 <91596170+DarrenDee96@users.noreply.github.com>
2021-10-05 15:49:24 +0100

    Instantiable Book

--- Lab5/exercise3/TestThermometer.java
+++ Lab5/exercise3/TestThermometer.java
@@ -7,13 +7,13 @@ public class TestThermometer {
 
         Thermometer t1 = new Thermometer();
 
-        t1.Thermometer(int the)
+
 
         JOptionPane.showMessageDialog(null , "****Thermometer Testing **** \n\n " +
                 "Calling the single argument constructor... setting the temperature of the first thermometer to 35C\n" +
                 "First Thermometer\n" );
 
 
-        temperature = JOptionPane.showInputDialog(null, "")
+        //temperature = JOptionPane.showInputDialog(null, "")
     }
 }
